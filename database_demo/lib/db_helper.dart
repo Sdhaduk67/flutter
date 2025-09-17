1ea6fb4bb50b633fcd15bbfd2ff030330ddb785f
mahadev <mahadev@DESKTOP-192B6UQ>
2025-09-17 10:31:43 +0530

    commit

---
+++ database_demo/lib/db_helper.dart
@@ -0,0 +1,33 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DbHelper {
+  Database? _db;
+  Future<Database> getDb() async {
+    if (_db != null) {
+      return _db!;
+    }
+    _db = await initDatabase();
+    return _db!;
+  }
+
+  Future<Database> initDatabase() async {
+    String databasePath = await getDatabasesPath();
+    String dbPath = join(databasePath, 'database_demo.db');
+
+    return await openDatabase(
+      dbPath,
+      version: 1,
+      onCreate: (db, version) => onCreate(db, version),
+    );
+  }
+
+  Future<void> onCreate(Database db, int version) async {
+    await db.execute('''
+    CREATE TABLE contact_master (
+      id PRIMARY KEY AUTOINCREMENT,
+      name TEXT,
+      mobile TEXT
+    )''');
+  }
+}
